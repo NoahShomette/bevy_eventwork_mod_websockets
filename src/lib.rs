2222e8b3243a8a8314f923b4bff8786bb5111345
Noah <noahshomette@gmail.com>
2024-06-30 21:26:32 -0400

    back to le bytes

--- src/lib.rs
+++ src/lib.rs
@@ -132,7 +132,7 @@ mod native_websocket {
                     Ok(8) => {
                         let bytes = &buffer[..8];
                         println!("{:?}", bytes);
-                        u64::from_be_bytes(
+                        u64::from_le_bytes(
                             bytes
                                 .try_into()
                                 .expect("Couldn't read bytes from connection!"),
